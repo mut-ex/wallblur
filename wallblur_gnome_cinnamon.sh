58f73e3249df411264c2bae4b5c1a406f2ade416
Dimitris Bounias <bounias.dimitris@gmail.com>
2019-10-05 03:00:42 +0300

    Restructure to support better both gnome and cinnamon

--- wallblur_gnome_cinnamon.sh
+++ wallblur_gnome_cinnamon.sh
@@ -3,36 +3,53 @@
 
 ### ---- Global variables ----
 
+### Find if cinnamon or gnome
+desktop="gnome"
+if grep -q innamon <<<"$XDG_CURRENT_DESKTOP"; then
+	desktop="cinnamon"
+fi
+package=org.$desktop.desktop.background # DE-specific
+
+### Screen resolution
+resolution=$(echo -n $(xrandr | grep '*') | awk '{print $1;}')
+
 ### Never changing
-curr_user=$(whoami)
-cache_dir="/home/"$curr_user"/.cache/wallblur"
-last_wallpaper_cache="$cache_dir/wallpaper.jpeg"
-gnome_wallpaper_cache="/home/${curr_user}/.cache/wallpaper"
-basefilename=$(basename -- "$last_wallpaper_cache")
+cache_dir=$HOME/.cache/wallblur
+last_wallpaper_file_path=$cache_dir/last_wallpaper_path
+
+### Current wallpaper info (also populated by settle_new_wallpaper)
+last_wallpaper_path=""
+if [ -f last_wallpaper_file_path ]; then
+	last_wallpaper_path=$(<$last_wallpaper_file_path)
+fi
+basefilename=$(basename -- "$last_wallpaper_path")
 extension="${basefilename##*.}"
 filename="${basefilename%.*}"
 
-# Will be populated/changed after the first loop iteration
-last_wallpaper=""
-prev_state=""
-wallpaper_set_by_this=false
+### Will be populated/changed after the first loop iteration
+prev_state="" # This shouldn't be neither blurred nor unblurred
 new_wallpaper=false
 
 
 ### ---- Helper functions ----
 
 ### Called when a new wallpaper is detected
+### It uses (the global) last_wallpaper_path to populate everything
 settle_new_wallpaper() {
 	### Remove old cache dir and recreate it
-	if [  "$(ls -A "$cache_dir")" ]; then
+	if [ -d "$cache_dir" ]; then
 		echo " * Cleaning existing cache..."
 		rm -r "$cache_dir"/*
 	fi
 	mkdir -p $cache_dir
 
-	### Copy the new wallpaper in cache as a jpeg
-	### (the original is a jpeg, but doesn't have the extension)
-	cp "$gnome_wallpaper_cache/$last_wallpaper" $last_wallpaper_cache
+	### Save the new uri to a file (to be checked upon restart of the pc)
+	echo $last_wallpaper_path > $last_wallpaper_file_path
+
+	### File info
+	basefilename=$(basename -- "$last_wallpaper_path")
+	extension="${basefilename##*.}"
+	filename="${basefilename%.*}"
 }
 
 ### usage: isMinimized <windowId>
@@ -54,22 +71,23 @@ number_of_unminimized_windows_in_current_workspace() {
 }
 
 gen_blurred_seq () {
-	### Send a notification
-	#notify-send "Generating blured wallpaper: "$basefilename"..."
+	### Resize to screen resolution
+	local tmp=""$cache_dir"/tmp."$extension""
+	convert -resize $resolution "$last_wallpaper_path" $tmp
 
 	### Generate incrementally blurred images
 	for i in $(seq 0 1 5); do
 		blurred_wallpaper=""$cache_dir"/"$filename""$i"."$extension""
-		convert -blur 0x$i $last_wallpaper_cache $blurred_wallpaper
+		### TODO: Linux mint default images are very large and it's slow -> Subsample
+		convert -blur 0x$i $tmp "$blurred_wallpaper"
 		echo " > Generating... $(basename $blurred_wallpaper)"
 	done
 }
 
-
 do_blur () {
 	for i in $(seq 5); do
 		blurred_wallpaper=""$cache_dir"/"$filename""$i"."$extension""
-		gsettings set org.gnome.desktop.background picture-uri file:///"$blurred_wallpaper"
+		gsettings set $package picture-uri file://"$blurred_wallpaper"
 
 	done
 }
@@ -77,7 +95,7 @@ do_blur () {
 do_unblur () {
 	for i in $(seq 5 -1 0); do
 		blurred_wallpaper=""$cache_dir"/"$filename""$i"."$extension""
-		gsettings set org.gnome.desktop.background picture-uri file:///"$blurred_wallpaper"
+		gsettings set $package picture-uri file://"$blurred_wallpaper"
 
 	done
 }
@@ -86,33 +104,49 @@ do_unblur () {
 ### ---- Main execution ----
 
 echo "Cache dir: $cache_dir"
+echo "Desktop Environment: $desktop"
 
-### If there is a wallpaper in cache, assume it was the last used
-### That's so that it doesn't consider the blurred as the normal
-### upon restart
-if [ -f "$last_wallpaper_cache" ]; then
-	wallpaper_set_by_this=true
-fi
+first_time=true
 
 ### Main loop
 while :; do
-	curr_wallpaper="$(ls -A $gnome_wallpaper_cache)"
-	
-	### Check if the wallpaper changed by this script
-	if [ "$wallpaper_set_by_this" = true ]; then
-		wallpaper_set_by_this=false
-		last_wallpaper=$curr_wallpaper
-	fi
+	### The gsettings command returns the path to the current wallpaper (with file:// at the start
+	### and wrapped in '' quotes)
+	### Note that it doesn't work for slideshows, for which it returns an xml containing all the paths
+	curr_wallpaper_path="$(gsettings get $package picture-uri)"
+	curr_wallpaper_path=$(echo -n "$curr_wallpaper_path" | tail -c +9 | head -c -1) # Remove file:// and quotes
 
-	### Check if the wallpaper has changed
-	if [ ! "$curr_wallpaper" = "$last_wallpaper" ]; then
+	# ### If it's the first time the script runs 
+	# ### and the current wallpaper was set by this script
+	# ### Then avoid the resetting it again for performance reasons
+	# if [first_time = true ] && [ "$curr_wallpaper_path" = file://$cache_dir/* ]; then
+	# 	last_wallpaper=file://$last_wallpaper_cached
+	# fi
+	# first_time=false
+
+	# ### If the execution gets inside this if, it means that 
+	# ### It's the first time it runs and
+	# ### the current wallpaper was set by this script (so the blurred versions have been generated) 
+	# ### but a blurred version incorrectly remained as the wallpaper (forced shutdown or similar)
+	# if [ first_time = true ]; then
+	# 	if [ "$curr_wallpaper_path" = file://$cache_dir/* ]; then
+	# 		curr_wallpaper_path=$last_wallpaper_cached # Don't keep the blurred version
+	# 	fi
+	# fi
+
+	### Check if the wallpaper has changed (-> the uri is outside our cache_dir)
+	# echo 1. $curr_wallpaper_path
+	# echo 2. $new_wallpaper
+	# echo 3. $cache_dir
+	if [[ ! "$curr_wallpaper_path" == "$cache_dir/"* ]] && [ $new_wallpaper = false ]; then
 		echo "* The wallpaper changed"
 		new_wallpaper=true
-		notify-send "wallblur: Processing new wallpaper... Please don't set a new one until this is finished"
-		last_wallpaper=$curr_wallpaper
+		#notify-send -t 3 "wallblur: Processing new wallpaper... Please don't set a new one until this is finished"
+		last_wallpaper_path=$curr_wallpaper_path
 		settle_new_wallpaper
 		gen_blurred_seq
 		prev_state="" # Reset
+		continue # So that it rechecks the current wallpaper before changing
 	fi
 
 	### Find if there are unminimized windows
@@ -124,11 +158,10 @@ while :; do
 		if [ "$prev_state" != "unblurred" ]; then
 			echo " ! Un-blurring"
 			do_unblur
-			wallpaper_set_by_this=true
 			prev_state="unblurred"
 			if [ "$new_wallpaper" = true ]; then
 				pkill notify-osd # Clear old notification
-				notify-send "wallblur: Wallpaper processed, you can now change wallpaper again without issues"
+				#notify-send -t 3 "wallblur: Wallpaper processed, you can now change wallpaper again without issues"
 				new_wallpaper=false
 			fi
 		fi
@@ -136,11 +169,10 @@ while :; do
 		if [ "$prev_state" != "blurred" ]; then
 			echo " ! Blurring"
 			do_blur
-			wallpaper_set_by_this=true
 			prev_state="blurred"
 			if [ "$new_wallpaper" = true ]; then
 				pkill notify-osd # Clear old notification
-				notify-send "wallblur: Wallpaper processed, you can now change wallpaper again without issues"
+				#notify-send -t 3 "wallblur: Wallpaper processed, you can now change wallpaper again without issues"
 				new_wallpaper=false
 			fi
 		fi
